684f7096c1a7257ec516a485d01855c194f7e2f9
fallenoak <git@fallenoak.me>
2023-01-01 15:53:51 -0600

    feat(instance): add TExtraInstanceRecyclable

---
+++ common/instance/TExtraInstanceRecyclable.hpp
@@ -0,0 +1,13 @@
+#ifndef COMMON_INSTANCE_T_EXTRA_INSTANCE_RECYCLABLE_HPP
+#define COMMON_INSTANCE_T_EXTRA_INSTANCE_RECYCLABLE_HPP
+
+#include <cstdint>
+
+template <class T>
+class TExtraInstanceRecyclable {
+    public:
+    // Member variables
+    uint32_t m_recycleBytes;
+};
+
+#endif
