f7b23143af95c69c294097b266a3b1f56266ade5
fallenoak <git@fallenoak.me>
2025-10-19 22:41:22 -0500

    chore(objectalloc): correct error macros in ObjectAllocAddHeap

--- common/ObjectAlloc.cpp
+++ common/ObjectAlloc.cpp
@@ -33,7 +33,9 @@ int32_t ObjectAlloc(uint32_t heapId, uint32_t* memHandle, void** objectPtr, bool
 }
 
 uint32_t ObjectAllocAddHeap(uint32_t objectSize, uint32_t objsPerBlock, const char* name, bool a4) {
-    STORM_ASSERT(objectSize > 0);
+    STORM_VALIDATE_BEGIN;
+    STORM_VALIDATE(objectSize > 0);
+    STORM_VALIDATE_END;
 
     auto globals = GetObjAllocGlobals();
 
